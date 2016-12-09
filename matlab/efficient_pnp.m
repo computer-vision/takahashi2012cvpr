475b1c2d010de2260ddd79bee6763c466ae788b7
takahashi.kosuke <takahashikosuke@MacBook-2.local>
2016-12-09 21:10:08 +0900

    modify efficient_pnp.m

--- matlab/efficient_pnp.m
+++ matlab/efficient_pnp.m
@@ -1,5 +1,7 @@
 function [R,T,Xc,best_solution]=efficient_pnp(x3d_h,x2d_h,A)
 
-fprintf('[MSG]: Please download EPnP code from http://cvlab.epfl.ch/EPnP/index.php and replace this function.');
+fprintf('[MSG]: Please download EPnP code from http://cvlab.epfl.ch/EPnP/index.php and replace this function.\n');
 
-exit;
+fprintf('[MSG]: Press Ctrl-C to abort.');
+
+pause(inf);
