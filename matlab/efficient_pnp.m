8c2b1f7c63c1c4ff4f168ec226149c7a6227fa82
Kosuke Takahashi <t.kousuke.1210@gmail.com>
2016-12-21 20:57:58 +0900

    efficient_pnp_gauss -> efficient_pnp

--- matlab/efficient_pnp.m
+++ matlab/efficient_pnp.m
@@ -1,4 +1,4 @@
-function [R,T,Xc,best_solution]=efficient_pnp_gauss(x3d_h,x2d_h,A)
+function [R,T,Xc,best_solution]=efficient_pnp(x3d_h,x2d_h,A)
 
 fprintf('[MSG]: Please download EPnP code from http://cvlab.epfl.ch/EPnP/index.php and replace this function.\n');
 
